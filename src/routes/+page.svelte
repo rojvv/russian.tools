1b9ad5397359f229e07f748fa5ba67c27a120a3c
Roj <rojvv@icloud.com>
2026-09-22 19:11:39 +0300

    k

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,8 @@ const i18n = getI18n();
 </nav>
 
 <p class="source">
-  &copy; 2026 russian.tools. <a href="https://t.me/RussianDotTools">Telegram</a>.
+  &copy; 2026 russian.tools. <a href="https://t.me/RussianDotTools"
+  >Telegram</a>.
   {i18n.t("openRussianAttribution")} <a href="https://en.openrussian.org/"
   >OpenRussian</a>.
 </p>
